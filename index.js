05981a7028bc644154fa3298693b0a7cc61f5dea
Aarron Lee <aclee91@gmail.com>
2019-10-17 19:48:13 -0400

    fix typos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getNews() {
   const apiKey = '3f519ed0127544559c348ae00b3ad4bf';
 
   return fetch(
-    `https://newsapi.org/v2/everything?q=bitcoin&from=2019-09-17&sortBy=publishedAt&apiKey=${apiKey}`
+    `https://newsapi.org/v2/everything?q=apple&from=2019-09-17&sortBy=publishedAt&apiKey=${apiKey}`
   )
     .then(res => res.json())
     .then(news => {
@@ -16,7 +16,7 @@ function renderCard({ title, imgLink, date }) {
       <img src="${imgLink}" alt="" class="card-img-top">
       <div class="card-body">
         <h5 class="card-title">${title}</h5>
-        <p class="card-text"><small class="text-muted>${date}</small></p>
+        <p class="card-text"><small class="text-muted">${date}</small></p>
       </div>
     </div>`;
 }
@@ -29,7 +29,7 @@ function renderNews({ news }) {
       (cols.innerHTML += renderCard({
         title: article.title,
         imgLink: article.urlToImage,
-        date: article.publishedDate
+        date: article.publishedAt
       }))
   );
 }
