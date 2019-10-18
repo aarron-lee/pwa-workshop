e1ff4fa6ca494be66844c96863fb33b5a270b0b5
Aarron Lee <aclee91@gmail.com>
2019-10-17 20:34:45 -0400

    sw cache checks

--- sw.js
+++ sw.js
@@ -6,5 +6,14 @@ const CACHED_FILES = ['/', '/index.html', '/index.css', '/index.js'];
 
 self.addEventListener('install', function(event) {
   console.log('SERVICE WORKER INSTALLED');
+  /* underneat the hood, cache.addAll caches the actual files */
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CACHED_FILES)));
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request);
+    })
+  );
 });
