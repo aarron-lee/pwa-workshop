5008a232ae26fe654a499eceb2346544762994c5
Aarron Lee <aclee91@gmail.com>
2019-10-17 21:36:24 -0400

    cache workbox dependency

--- sw.js
+++ sw.js
@@ -18,8 +18,15 @@ if (workbox) {
     })
   );
   workbox.routing.registerRoute(
-    'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js',
-    new workbox.strategies.CacheFirst()
+    new RegExp('^https://storage.googleapis.com/'),
+    new workbox.strategies.CacheFirst({
+      cacheName: 'workbox-sw-cache',
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200]
+        })
+      ]
+    })
   );
 
   workbox.routing.registerRoute(/\.(?:js|css|html)$/, new workbox.strategies.CacheFirst());
