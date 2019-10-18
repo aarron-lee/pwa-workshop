a5cb00d5fca88d261193a1b8e56cb6b1ed47eba9
Aarron Lee <aclee91@gmail.com>
2019-10-17 20:37:08 -0400

    persist to cache if not in cache

--- sw.js
+++ sw.js
@@ -6,14 +6,27 @@ const CACHED_FILES = ['/', '/index.html', '/index.css', '/index.js'];
 
 self.addEventListener('install', function(event) {
   console.log('SERVICE WORKER INSTALLED');
-  /* underneat the hood, cache.addAll caches the actual files */
+  /* underneath the hood, cache.addAll caches the actual files */
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CACHED_FILES)));
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.match(event.request);
+      return cache.match(event.request).then(response => {
+        /* the response is undefined if it isn't in the cache
+         the || will fall back to the actual fetch event
+        */
+        return (
+          response ||
+          fetch(event.request).then(response => {
+            /* if the response was successful, then cache it with cache.put(key, value) */
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+          })
+        );
+      });
     })
   );
 });
